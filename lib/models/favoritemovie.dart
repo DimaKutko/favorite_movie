e84b34742915997f0b2af60a18905ad649327cd9
DimaKutko <56451967+DimaKutko@users.noreply.github.com>
2020-01-05 01:45:27 +0200

    update favoritemovie class

--- lib/models/favoritemovie.dart
+++ lib/models/favoritemovie.dart
@@ -1,6 +1,6 @@
 class FavoriteMovie {
   String id;
-   String title;
+  String title;
   String year;
   String poster;
   String label;
@@ -9,30 +9,18 @@ class FavoriteMovie {
   int rating;
   int timestamp;
 
-  FavoriteMovie(
-      this.id,
-      this.title,
-      this.year,
-      this.poster,
-      this.label,
-      this.priority,
-      this.viewed,
-      this.rating,
-      this.timestamp);
+  FavoriteMovie(this.id, this.title, this.year, this.poster, this.label,
+      this.priority, this.viewed, this.rating, this.timestamp);
 
-  factory FavoriteMovie.fromJson(Map<String, dynamic> json) {
-    
-      id = json['id'];
-      title = json['title'];
-      year = json['year'];
-      poster = json['poster'];
-      label = json['label'];
-      priority = json['priority'] as int;
-      viewed = json['viewed'] as bool;
-      rating = json['rating'] as int;
-      timestamp = json['timestamp'] as int;
-    
+  FavoriteMovie.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    title = json['title'];
+    year = json['year'];
+    poster = json['poster'];
+    label = json['label'];
+    priority = json['priority'] as int;
+    viewed = json['viewed'] as bool;
+    rating = json['rating'] as int;
+    timestamp = json['timestamp'] as int;
   }
-  
-  
 }
